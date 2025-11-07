f185e720d0c33e5c6b35a79e03523fad522ebcbc
Michael <mt.dev.2023@gmail.com>
2025-11-07 14:11:49 +0100

    Exercice 1 : fonction pairNumbers terminée

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,17 @@
-//votre code ici
+function pairNumbers(min, max) {
+  const pairs = [];
 
-export default pairNumbers
+  for (let n = min; n <= max; n++) {
+    // si n est pair, on l'ajoute au tableau
+    if (n % 2 === 0) {
+      pairs.push(n);
+    }
+  }
+
+  // transforme le tableau [2,4,6] en "2,4,6"
+  return pairs.join(",");
+}
+
+console.log(pairNumbers(1, 10));
+
+// export default pairNumbers;
