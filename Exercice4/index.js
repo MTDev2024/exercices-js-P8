858736e80e63e8e6d0ffb1a65a7f6f2532c3d659
Michael <mt.dev.2023@gmail.com>
2025-11-08 12:49:05 +0100

    feat(Exercice5): amélioration ’accessibilité de la page HTML (labels, alt, main, lien)

--- Exercice4/index.js
+++ Exercice4/index.js
@@ -1,10 +1,10 @@
 const myButton = document.getElementById("myButton");
 
 myButton.addEventListener("click", function () {
-  // Vérifier si le paragraphe existe déjà
+  // Vérifier si p existe déjà
   if (!document.getElementById("message")) {
     const p = document.createElement("p");
-    p.id = "message"; // Donner un id pour pouvoir le retrouver
+    p.id = "message"; // id pour pouvoir le retrouver
     p.textContent = "Bonjour, vous avez cliqué sur le bouton !";
     myButton.parentNode.appendChild(p);
   }
